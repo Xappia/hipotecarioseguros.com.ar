2b551c453e52bebbb5248b29f454dfcb671ed751
jespinoza <juespinozacorrea@gmail.com>
2018-04-13 12:45:26 -0300

    validacion de longitud 10 en numero y cod de area

--- cotizar-seguro-hogar/1/assets/js/init.js
+++ cotizar-seguro-hogar/1/assets/js/init.js
@@ -4,25 +4,22 @@ $(document).ready(function(e) {
 	$("#00NQ0000001wsEB").blur(function() {
 		var area = $("#00NQ0000001wsEB").val();
 		$("#00NQ0000001wsEB").val(area.replace(/^0+/, ''));
-        validate_number_length();
 	});
 
-	$("#phone").blur(validate_number_length()).keyup(function(){
-        $("#btn_enviar").prop("disabled", true);
-        var maxLength = 10;
-        var completeNumber = $("#00NQ0000001wsEB").val() + $("#phone").val();
-        if(completeNumber.length == maxLength){
-            $("#btn_enviar").prop("disabled", false);
-        }
+	$("#phone").blur(function(){
+		(validate_number_length())?$("#phone-error").hide():$("#phone-error").show();
+	}).keyup(function(){
+        (validate_number_length())?$("#btn_enviar").prop("disabled", false):$("#btn_enviar").prop("disabled", true);
     });
 
 	function validate_number_length(){
-        $("#phone-error").show();
+        var valid = false;
         var maxLength = 10;
         var completeNumber = $("#00NQ0000001wsEB").val() + $("#phone").val();
         if(completeNumber.length == maxLength){
-            $("#phone-error").hide();
+            valid = true;
         }
+        return valid;
 	}
 
 	$("#formulario").validator().on('submit', function(e) {

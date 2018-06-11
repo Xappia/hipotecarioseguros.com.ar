1d816ddc781a5f571aa9f134efed91960f3100e1
ADXappia <>
2018-06-11 11:55:52 -0300

    SEGCRM-1011 Cambios en los formularios de todas las preguntas

--- cotizar-seguro-hogar/sitio/pregunta-16.php
+++ cotizar-seguro-hogar/sitio/pregunta-16.php
@@ -144,6 +144,80 @@
         </div>
       </div>
     </div>
+	
+	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '133952657253150');
+	  fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	  src="https://www.facebook.com/tr?id=133952657253150&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+
+	<!-- Facebook Pixel Code -->
+	<script>
+	  !function(f,b,e,v,n,t,s)
+	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	  n.queue=[];t=b.createElement(e);t.async=!0;
+	  t.src=v;s=b.getElementsByTagName(e)[0];
+	  s.parentNode.insertBefore(t,s)}(window, document,'script',
+	  'https://connect.facebook.net/en_US/fbevents.js');
+	  fbq('init', '133952657253150');
+	  fbq('track', 'PageView');
+	  fbq('track', 'CompleteRegistration');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+					src="https://www.facebook.com/tr?id=133952657253150&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+
+	<!-- Google Code para etiquetas de remarketing -->
+	<!--------------------------------------------------
+	Es posible que las etiquetas de remarketing todavía no estén asociadas a la información personal identificable o que estén en páginas relacionadas con las categorías delicadas. Para obtener más información e instrucciones sobre cómo configurar la etiqueta, consulte http://google.com/ads/remarketingsetup.
+	--------------------------------------------------->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+		var google_conversion_id = 832097181;
+		var google_custom_params = window.google_tag_params;
+		var google_remarketing_only = true;
+		/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+		<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/832097181/?guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+
+	<!-- Google Code for Conversi&oacute;n Hipotecario Seguros Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+		var google_conversion_id = 832097181;
+		var google_conversion_label = "u-PVCIf0hXYQnZfjjAM";
+		var google_remarketing_only = false;
+		/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/832097181/?label=u-PVCIf0hXYQnZfjjAM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+
+	
     <!-- Bootstrap core JavaScript
       ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
